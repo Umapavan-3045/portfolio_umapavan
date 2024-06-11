e083e7dc93b419a1971d2ddd1fa8fc32c30edb17
Umapavan-3045 <132034901+Umapavan-3045@users.noreply.github.com>
2024-06-11 21:07:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// main.js
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "App Developer", "Software Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+  
